f8a56bca1600fcf799350d34d886c18f2034bf1e
Cos <cosjava26@gmail.com>
2019-05-08 01:13:07 +0800

    春遊內頁 navigation bar

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,8 +1,30 @@
 <template>
-    <div class="header">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <header class="navbar sticky-top navbar-expand-lg navbar-light bg-transparent">
+        <div class="container">
+            <router-link class="navbar-brand" to="/">
+              <img src="../../assets/images/logo.svg" alt="台北小春">
+            </router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample07">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item" v-for="(item, index) in menu" :key="index"
+                    :class="{ 'active': ( '/' + item.link ) === searchText}"
+                    >
+                        <router-link class="nav-link" :to='item.link'>{{ item.name }}</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- <div class="header">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-transparent">
             <div class="container">
-                <router-link class="navbar-brand" to="/">台北小春</router-link>
+                <router-link class="navbar-brand" to="/">
+                  <img src="../../assets/images/logo.svg" alt="台北小春">
+                </router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -18,7 +40,7 @@
                 </div>
             </div>
         </nav>
-    </div>
+    </div> -->
 </template>
 <script>
 import menujson from '../../../static/data/menu.json'
@@ -42,5 +64,7 @@ export default {
 }
 </script>
 <style lang="scss">
-
+  .bg-transparent{
+    background: rgba(255,255,255,0);
+  }
 </style>
